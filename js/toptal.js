3c50e81f875d0ff7c0896f9c3018b7ff77133cc8
swechchhaparajuli <swechchha@gmail.com>
2019-04-01 01:42:55 -0700

    formatted

---
+++ js/toptal.js
@@ -0,0 +1,56 @@
+
+
+
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("c");
+var d = document.getElementById("d");
+var e = document.getElementById("e");
+var mission = document.getElementById("mission");
+var idea = document.getElementById("idea");
+
+a.textContent = "";
+b.textContent = "";
+c.textContent = "";
+d.textContent = "";
+e.textContent = "";
+false.textContent = "";
+mission.textContent = "";
+
+
+
+$("#misStat").hover(function(){
+    mission.textContent = "I am an undergraduate at UC Santa Cruz majoring in Computer Science. While I had always been very active in social activism and participating in social issues along with being very into the subject of my major, I had never found a way to bring those two together. With a lot of self-studying and a course at the UC Berkeley extension I started working on my own projects, navigating noSQL databases like Mongoose with Node.js for side project practice and finally to implement on my own web project of organizing sponsors with the 12 kids in Nepal that will now be attending a boarding school until they reach college. This organized sponsorship for these kids grew from just being able to take them out of that terrible situation in the orphanage to giving them a school where they can live and eat alongside with sponsors and mentors provided through my site. With the toptal $10,000, I would finish up my undergraduate degree which is just one more year. While working on this project I completed a few lower-division requirements for my major at a local community college while also finishing the UC Berkeley extension course on Full-Stack Web Development. However, I now have only my upper-division courses on Computer Science left before I complete my undergraduate degree. With my degree, I will have the knowledge and access to tools which will help me make a greater impact and will also help me greatly in continuing this project in a larger scale. The lack of educational opportunities in Nepal for children, especially girls, is appalling and having the knowledge to work with that data to create a better and more secure tool is my goal.";
+    a.textContent = "";
+    b.textContent = "";
+    c.textContent = "";
+    d.textContent = "";
+    e.textContent = "";
+});
+
+$("#theWeb").hover(function(){
+    a.textContent = "We do not receive any money or materials from the sponsors, instead, each sponsor is provided with information on how to communicate with the bank wiring information of the actual boarding school that the students are enrolled in. We provide the contact information of the school to the sponsor, so that they can communicate about the child they are sponsoring to see how they are doing. The main focus of this website is to establish a connection between the sponsor, the student, and their school. It is not a fundraising tool.";
+        
+    b.textContent = "If you are interested in sponsoring, the website shows which of the students still need a sponsor. Additionally, it also includes videos of each student talking about their points of interest and what they want to do when they grow up. ";
+    c.textContent = "";
+    d.textContent = "";
+    e.textContent = "";
+});
+
+$("#futDev").hover(function(){
+    c.textContent = "Implement an authorization system, through google authentication so that the Sponsors can see the status of the student that they are sponsoring. This should provide them with the monthly or yearly tuition of the student as well as tell them when they need to pay.";
+    d.textContent = "Starting this project made me realize that I could combine my strengths to make a real contribution in education. These kids aren’t the only ones without access to school, and scaling this project up from a focused 12-student website to a tool that is much larger would be incredible. There are many children around the world in need of education, and there are many people who have the means to provide them with one. Creating a space that cuts out the middleman that stands to profit between a child and their education is what I really hope to build.";
+    a.textContent = "";
+    b.textContent = "";
+    e.textContent = "";
+});
+
+$("#whyImp").hover(function(){
+    e.textContent = "Education can be incredibly inaccessible, and higher education is often used as an elitist tool. Those who haven’t had access to education remain pushed down in the socioeconomic ladder whereas those who have power and privilege continue to rise. Occasionally, somebody pushes past this but one or two people rising through the adversity is not enough. There was a time in my life where I didn’t have a free-flowing access to the internet, and when my parents could barely pay for me to go to school. Additionally, as a woman, I’m lucky that my education has always been valued just as much as my brother’s which is not the case for a lot of women around the world. Through barely scraping by to pay for a private school education in Nepal, to becoming struggling immigrants in The United States my parents put my access to a good education above all other needs. At a young age, I was blind to the how much I took for granted but as I learned about the oppression that others are faced with I began to see how much I could do from the position that I had found myself to be in. ";
+    f.textContent = "Starting this project made me realize that I could combine my strengths to make a real contribution in education. These kids aren’t the only ones without access to school, and scaling this project up from a focused 12-student website to a tool that is much larger would be incredible. There are many children around the world, or even just in that one city in need of education, and there are many people who have the means to provide them with one.";
+    a.textContent = "";
+    b.textContent = "";
+    c.textContent = "";
+    d.textContent = "";
+});
+
